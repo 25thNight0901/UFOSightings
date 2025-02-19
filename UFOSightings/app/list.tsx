aed79ea838a124785a4c8ab36777d079c3ee1e34
RodrigoUma04 <151837337+RodrigoUma04@users.noreply.github.com>
2025-02-19 14:51:20 +0100

    Implemented list fetching and generation

--- UFOSightings/app/list.tsx
+++ UFOSightings/app/list.tsx
@@ -1,3 +1,40 @@
-const List = () => {};
+import React, {useEffect, useState} from "react";
+import { ISighting} from "../types";
+import {View, Button, Text, ActivityIndicator, FlatList} from "react-native";
+import Constants from "expo-constants";
+
+const List = () => {
+    const [results, setResults] = useState<ISighting[]>([]);
+
+    useEffect(()=>{
+       const loadData = async() => {
+        let result = await fetch("https://sampleapis.assimilate.be/ufo/sightings");
+        let json : ISighting[] = await result.json();
+
+        setResults(json);
+        }
+        loadData();
+    },[]);
+    
+    return (
+        <View style={{flexDirection: "column", flex: 1, paddingTop: Constants.statusBarHeight}}>
+            <FlatList
+                data={results}
+                renderItem={({item}) => <PersonComponent item={item}/>}
+                keyExtractor={item => item.id.toString()}
+            />
+        </View>
+    )
+
+};
+
+
+const PersonComponent = ({item} : {item: ISighting}) => {
+    return (
+        <View style={{flexDirection: "row", alignItems: "center"}}>
+            <Text style={{flex: 1}}>{item.id}</Text>
+        </View>
+    )
+}
 
 export default List;
