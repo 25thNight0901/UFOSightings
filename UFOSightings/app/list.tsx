ec17d49e77847fd0a5898a0f6c4d3e6ab87303c6
RodrigoUma04 <151837337+RodrigoUma04@users.noreply.github.com>
2025-02-19 15:54:35 +0100

    Create list format; Make every item pressable.

--- UFOSightings/app/list.tsx
+++ UFOSightings/app/list.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { ISighting} from "../types";
-import {View, Button, Text, ActivityIndicator, FlatList} from "react-native";
+import {View, Button, Text, ActivityIndicator, FlatList, Pressable} from "react-native";
 import Constants from "expo-constants";
 
 const List = () => {
@@ -18,6 +18,13 @@ const List = () => {
     
     return (
         <View style={{flexDirection: "column", flex: 1, paddingTop: Constants.statusBarHeight}}>
+            <Text style={{fontSize: 30, fontWeight: 700}}>List of sightings</Text>
+            <View style={{ flexDirection: "row", alignItems: "center", backgroundColor: "#ddd", padding: 10 }}>
+                <Text style={{ flex: 1, fontWeight: "bold" }}>ID</Text>
+                <Text style={{ flex: 1, fontWeight: "bold" }}>Witness</Text>
+                <Text style={{ flex: 1, fontWeight: "bold" }}>Status</Text>
+                <Text style={{ flex: 1, fontWeight: "bold" }}>Date/Time</Text>
+            </View>
             <FlatList
                 data={results}
                 renderItem={({item}) => <PersonComponent item={item}/>}
@@ -31,8 +38,13 @@ const List = () => {
 
 const PersonComponent = ({item} : {item: ISighting}) => {
     return (
-        <View style={{flexDirection: "row", alignItems: "center"}}>
-            <Text style={{flex: 1}}>{item.id}</Text>
+        <View>
+            <Pressable style={{flexDirection: "row", alignItems: "center", padding: 10}}>
+                <Text style={{flex: 1}}>{item.id}</Text>
+                <Text style={{flex: 1}}>{item.witnessName}</Text>
+                <Text style={{flex: 1}}>{item.status}</Text>
+                <Text style={{flex: 1}}>{item.dateTime.toString()}</Text>
+            </Pressable>
         </View>
     )
 }
